9b7c4f357b86cad8f987eb9ef095055c0993c0e7
pcc-website <website@coventrypca.church>
2023-05-24 20:04:44 -0700

    feat: giving page

---
+++ src/pages/Giving.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout background="beliefs-blur.jpg">
+    <template slot="title">
+      <h1 class="title is-size-1">Giving</h1>
+    </template>
+    <div class="text-center">
+      <main class="content">
+        <div class="relative">
+          <iframe
+            id="v-frame"
+            width="100%"
+            frameborder="0"
+            allow="clipboard-write"
+            src="https://secure.myvanco.com/L-Z9Y3"
+            scrolling="no"
+          ></iframe>
+        </div>
+      </main>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@/layouts/Page.vue'
+
+// iframe sizing
+window.onmessage = (e) => {
+  const n = document.getElementById('v-frame')
+  'https://secure.myvanco.com' === e.origin &&
+    e.data.height &&
+    ((n.height = e.data.height), (n.overflow = 'hidden'))
+}
+
+export default {
+  components: {
+    Layout,
+  },
+}
+</script>
