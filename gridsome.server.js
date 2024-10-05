fb2b8c879a45079c57086643a9d76796954dd7b3
benonymity <ben@benbassett.dev>
2024-10-05 17:39:53 -0400

    feat: switch to google drive

--- gridsome.server.js
+++ gridsome.server.js
@@ -7,132 +7,130 @@
 
 const nodeExternals = require('webpack-node-externals')
 const _ = require('lodash')
-const AWS = require('aws-sdk')
+const { google } = require('googleapis')
 
 async function addBulletinNodes({ collection }) {
-	const S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID
-	const S3_SECRET = process.env.S3_SECRET
+  const DRIVE_FOLDER_ID = '1VKvQM2kQgzwB3Btk-v5ges0uQzp1Fn7t'
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  })
 
-	const s3 = new AWS.S3({
-		endpoint: new AWS.Endpoint('s3.wasabisys.com'),
-		region: 'us-east-1',
-		accessKeyId: S3_ACCESS_KEY_ID,
-		secretAccessKey: S3_SECRET,
-		params: {
-			Bucket: 'coventrypca.church',
-			Prefix: 'bulletins/',
-		},
-	})
+  const drive = google.drive({ version: 'v3', auth })
+  const res = await drive.files.list({
+    q: `'${DRIVE_FOLDER_ID}' in parents and mimeType='application/pdf'`,
+    fields: 'files(id, name)',
+    spaces: 'drive',
+  })
 
-	const res = await s3.listObjectsV2().promise()
+  for (const file of res.data.files) {
+    let title = file.name.replace('.pdf', '')
+    let date = /(\d{4}-\d{2}-\d{2})/.exec(title)[1]
+    let url = `https://drive.google.com/file/d/${file.id}/view`
 
-	let title, date, url
-	for (const { Key: file } of res.Contents) {
-		if (/.pdf$/.test(file)) {
-			title = file.replace('bulletins/', '').replace('.pdf', '')
-			date = /(\d{4}-\d{2}-\d{2})/.exec(title)[1]
-			url = 'https://s3.wasabisys.com/coventrypca.church/' + file
-
-			collection.addNode({
-				title,
-				date,
-				url,
-			})
-		}
-	}
+    collection.addNode({
+      title,
+      date,
+      url,
+    })
+  }
 }
 
-module.exports = function(api) {
-	// CREATE SCRIPTURE PAGES
-	api.createPages(async ({ graphql, createPage }) => {
-		const { data } = await graphql(`
-			{
-				allSermon {
-					edges {
-						node {
-							title
-							audio
-							video
-							scripture
-							series {
-								title
-								path
-								thumbnail(width: 400)
-							}
-							preacher {
-								name
-								path
-							}
-							path
-							date(format: "MMMM DD, YYYY")
-						}
-					}
-				}
-			}
-		`)
+module.exports = function (api) {
+  // CREATE SCRIPTURE PAGES
+  api.createPages(async ({ graphql, createPage }) => {
+    const { data } = await graphql(`
+      {
+        allSermon {
+          edges {
+            node {
+              title
+              audio
+              video
+              scripture
+              series {
+                title
+                path
+                thumbnail(width: 400)
+              }
+              preacher {
+                name
+                path
+              }
+              path
+              date(format: "MMMM DD, YYYY")
+            }
+          }
+        }
+      }
+    `)
 
-		let bibleBooks = await require('./src/assets/data/bible-books.json')
-		bibleBooks = _.flatMapDeep(bibleBooks)
+    let bibleBooks = await require('./src/assets/data/bible-books.json')
+    bibleBooks = _.flatMapDeep(bibleBooks)
 
-		for (const book of bibleBooks) {
-			var bookSlug = book.toLowerCase().replace(/\ /g, '-')
+    for (const book of bibleBooks) {
+      var bookSlug = book.toLowerCase().replace(/\ /g, '-')
 
-			let bookSermons = data.allSermon.edges.filter(({ node }) => {
-				return node.scripture.includes(book)
-			})
+      let bookSermons = data.allSermon.edges.filter(({ node }) => {
+        return node.scripture.includes(book)
+      })
 
-			createPage({
-				path: `/sermons/scripture/${bookSlug}`,
-				component: './src/templates/ScripturePage.vue',
-				context: {
-					id: bookSlug,
-					fields: {
-						title: book,
-						sermons: bookSermons,
-						type: 'ScripturePage',
-					},
-				},
-			})
-		}
-	})
+      createPage({
+        path: `/sermons/scripture/${bookSlug}`,
+        component: './src/templates/ScripturePage.vue',
+        context: {
+          id: bookSlug,
+          fields: {
+            title: book,
+            sermons: bookSermons,
+            type: 'ScripturePage',
+          },
+        },
+      })
+    }
+  })
 
-	// CREATE BULLETINS
-	api.loadSource(async store => {
-		const bulletins = store.addCollection({
-			typeName: 'Bulletin',
-		})
+  // CREATE BULLETINS
+  api.loadSource(async (store) => {
+    const bulletins = store.addCollection({
+      typeName: 'Bulletin',
+    })
 
-		await addBulletinNodes({ collection: bulletins })
-	})
+    await addBulletinNodes({ collection: bulletins })
+  })
 
-	// ADD LASTNAME TO PREACHER
-	api.loadSource(({ addSchemaResolvers }) => {
-		addSchemaResolvers({
-			Preacher: {
-				lastName: {
-					type: 'String',
-					resolve: src => {
-						let nameArray = src.name.split(' ')
-						return `${nameArray[nameArray.length - 1]}`
-					},
-				},
-			},
-		})
-	})
+  // ADD LASTNAME TO PREACHER
+  api.loadSource(({ addSchemaResolvers }) => {
+    addSchemaResolvers({
+      Preacher: {
+        lastName: {
+          type: 'String',
+          resolve: (src) => {
+            let nameArray = src.name.split(' ')
+            return `${nameArray[nameArray.length - 1]}`
+          },
+        },
+      },
+    })
+  })
 
-	api.chainWebpack((config, { isServer }) => {
-		if (isServer) {
-			config.externals([
-				nodeExternals({
-					allowlist: [
-						/^buefy/,
-						/^bulma/,
-						/^typeface-montserrat/,
-						/^tailwind/,
-						/^vuetify/,
-					],
-				}),
-			])
-		}
-	})
+  api.chainWebpack((config, { isServer }) => {
+    if (isServer) {
+      config.externals([
+        nodeExternals({
+          allowlist: [
+            /^buefy/,
+            /^bulma/,
+            /^typeface-montserrat/,
+            /^tailwind/,
+            /^vuetify/,
+          ],
+        }),
+      ])
+    }
+  })
 }
